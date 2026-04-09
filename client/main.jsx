8435fdb07ab87e1f5e389a282ff4baf41f4c8a6f
Rhuan <rhucambor@gmail.com>
2026-04-09 09:28:38 -0300

    Instalando pacotes e criando tema global. close #2

--- client/main.jsx
+++ client/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
 import { App } from "/imports/ui/App";
-import "/imports/ui/styles.css";
+import "./main.css";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+  <App />
+);
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,52 @@
-import { Counter } from "./Counter.jsx";
-import { Header } from "./Header.jsx";
-import { Info } from "./Info.jsx";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {Box, Typography, Button, Chip} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  "palette": {
+    "primary": {
+      "main": "#9dc0e6"
+    },
+    "mode": "dark",
+    "background": {
+      "default": "#1d1c1c",
+      "paper": "#00020d"
+    },
+    "secondary": {
+      "main": "#e6cf00"
+    },
+    "error": {
+      "main": "#830b00"
+    },
+    "warning": {
+      "main": "#ffa753"
+    },
+    "info": {
+      "main": "#c5ffeb"
+    },
+    "success": {
+      "main": "#51ff5b"
+    }
+  },
+  "typography": {
+    "fontSize": 13,
+    "fontFamily": "Roboto, Helvetica, Arial, sans-serif"
+  },
+  "shape": {
+    "borderRadius": 8
+  },
+  "spacing": 9
+});
 
 export const App = () => (
-  <div className="page">
-    <Header />
-    <main className="main">
-      <Counter />
-      <Info />
-    </main>
-  </div>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Box sx={{p: 2, display: "flex", flexDirection: "column", alignItems: "center", gap: 2}}>
+      <Button variant="outlined" color="secondary">
+        Testando estilos
+      </Button>
+      <Typography variant="h2">Princípio do Projeto</Typography>
+      <Chip label="Teste" color="error" onClick={() => {}} />
+    </Box>
+  </ThemeProvider>
 );
