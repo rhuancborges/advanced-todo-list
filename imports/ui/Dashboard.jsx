e3fd00813f451e374bafa2f5bdb180eccab3391a
Rhuan <rhucambor@gmail.com>
2026-04-14 07:54:46 -0300

    Construindo cards do Dashboard

--- imports/ui/Dashboard.jsx
+++ imports/ui/Dashboard.jsx
@@ -1,25 +1,62 @@
 import { Card, Typography, Box, Stack, Grid, CardContent } from "@mui/material";
 import {theme} from "./theme"
+import { useNavigate } from "react-router";
 
 export const Dashboard = () => {
+    const navigate = useNavigate();
+    const sxCard = {
+        minHeight: 260,
+        height: "100%",
+        width: 280,
+        borderRadius: 3,
+        display: "flex",
+        alignItems: "flex-star",
+        justifyContent: "center",
+        "&:hover": {
+        transform: "translateY(-4px)",
+        boxShadow: 6,
+        }
+    }
     return (
-        <Box sx={{marginLeft: 50, marginTop: 9}}>
-            <Grid container rowSpacing={4} columnSpacing={5} >
-                <Card>
-                    <CardContent>1</CardContent>
-                </Card>
-                <Card>
-                    <CardContent>2</CardContent>
-                </Card>
-            </Grid>
-            
-            <Grid container sx={{marginTop: 2}}rowSpacing={4} columnSpacing={5} >
-                <Card>
-                    <CardContent>1</CardContent>
-                </Card>
-                <Card>
-                    <CardContent>2</CardContent>
-                </Card>
+        <Box sx={{marginLeft: 35}}>
+            <Typography variant="h5" sx={{marginLeft:20, marginBottom: 2}}> Olá, seja bem vindo ao To-Do List!</Typography>
+            <Grid container rowSpacing={4} columnSpacing={1}>
+                <Grid item size={5}>
+                    <Card elevation={4}
+                        sx={sxCard}>
+                        <CardContent>
+                            <Typography variant="h4">Total de tarefas cadastradas</Typography>
+                            <Typography variant="h2" sx={{marginTop: 5}}>1</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item size={5}>
+                    <Card elevation={4}
+                        sx={sxCard}>
+                        <CardContent>
+                            <Typography variant="h4">Total de tarefas concluídas</Typography>
+                            <Typography variant="h2" sx={{marginTop: 5}}>2</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item size={5}>
+                   <Card elevation={4}
+                        sx={sxCard}>
+                        <CardContent>
+                            <Typography variant="h4">Total de tarefas a serem concluídas</Typography>
+                            <Typography variant="h2" sx={{marginTop: 5}}>3</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item size={5}>
+                    <Card elevation={4}
+                        sx={{...sxCard, "&:hover":{
+                            ...sxCard["&:hover"], cursor:"pointer"}}} onClick={()=>navigate("/home/view")}>
+                        <CardContent>
+                            <Typography variant="h3" sx={{marginTop: 5}}>Visualizar tarefas</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
         </Box>
     );
